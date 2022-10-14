8b1e28193f68a6ce73a0a58cf10403c1f8678032
Андрей Пестунов <andreypestunov@gmail.com>
2022-10-14 23:54:14 +0200

    add maui

---
+++ src/frontend/clients/AM.Clients.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AM.Clients.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
